391a2073fe342ba72de5783a97d9fcb56edff0a5
alexey-korablin <alex8456@bk.ru>
2016-06-24 18:17:07 +0300

    003 Added invisibility of information blocks, animation of blocks. Block is in viewport become visible. Added responsibility for less than 600px, from 600 to 900px, and more than 900px

---
+++ vertical_timeline/lib/script/show_text.js
@@ -0,0 +1,59 @@
+(function() {
+	'use strict';
+
+	var items = document.querySelectorAll('.timeline li');
+
+	function showText(el) {
+		var rect = el.getBoundingClientRect();
+		return (rect.top >= 0 && 
+			rect.left >= 0 && 
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight));
+	}
+
+	function callbackFunc() {
+		for(var i = 0; i < items.length; i++) {
+			if(showText(items[i])) {
+				items[i].classList.add('in-view');
+			}
+		}
+	}
+
+	window.addEventListener('load', callbackFunc);
+	window.addEventListener('scroll', callbackFunc);
+})();
+/*
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+*/
